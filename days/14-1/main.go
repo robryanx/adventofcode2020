fc59c60359a3aa880d2e45b69a249a89ee10b5ba
Rob Ryan <rob@affclicks.com>
2021-11-14 22:30:19 +1100

    Day 14

--- days/14-1/main.go
+++ days/14-1/main.go
@@ -2,8 +2,9 @@ package main
 
 import (
     "fmt"
-    "strings"
+    //"strings"
     "strconv"
+    "regexp"
     "adventofcode/2020/modules/readinput"
 )
 
@@ -14,9 +15,62 @@ func check(e error) {
 }
 
 func main() {
+    // mask = 1001X0X00110011X01X1000110100011000X
+    r_mask, _ := regexp.Compile("mask\\s\\=\\s([10X]{36})");
+
+    // mem[9042] = 12448
+    r_mem, _ := regexp.Compile("mem\\[([0-9]+)\\]\\s=\\s([0-9]+)");
+
     current_mask := ""
-    memory := map[int]int{}
+    memory := map[int]int64{}
     for _, row := range readinput.ReadStrings("inputs/14/input.txt", "\n") {
-        // check to see if the row is a value of a mask
+        mask_matches := r_mask.FindStringSubmatch(row);
+        if mask_matches != nil {
+            current_mask = mask_matches[1]
+        } else {
+            mem_matches := r_mem.FindStringSubmatch(row);
+            if mem_matches != nil {
+                pos, err := strconv.Atoi(mem_matches[1])
+                check(err)
+
+                raw_val, err := strconv.ParseInt(mem_matches[2], 10, 64)
+                check(err)
+
+                val := apply_mask(raw_val, current_mask)
+
+                memory[pos] = val
+            }
+        }
     }
+
+    var total int64
+    for _, val := range memory {
+        total += val
+    }
+
+    fmt.Println(total)
+}
+
+func apply_mask(value int64, mask string) int64 {
+    for i:=35; i>=0; i-- {
+        if mask[i] == '1' {
+            value = setBit(value, uint(35-i))
+        } else if mask[i] == '0' {
+            value = clearBit(value, uint(35-i))
+        }
+    }
+
+    return value
+}
+
+func setBit(n int64, pos uint) int64 {
+    n |= (1 << pos)
+    return n
+}
+
+func clearBit(n int64, pos uint) int64 {
+    var mask int64
+    mask = ^(1 << pos)
+    n &= mask
+    return n
 }
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go build -o ~/go/src/adventofcode/2020/bin/$1 days/$1/*
+go build -o ~/adventofcode/2020/bin/$1 days/$1/*
 
 if [ $? -eq 0 ]
 then
-    ~/go/src/adventofcode/2020/bin/$1;
+    ~/adventofcode/2020/bin/$1;
 fi
